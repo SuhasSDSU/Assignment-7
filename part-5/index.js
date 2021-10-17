f01aa008a484096d9288c126d821535cfd83a3a3
SuhasSDSU <spindiproli4798@sdsu.edu>
2021-10-17 12:01:52 -0700

    Updated Part-5

--- part-5/index.js
+++ part-5/index.js
@@ -6,31 +6,26 @@ function displayMessage() {
 }
 
 // Element Property
-var myElement = document.getElementById("myButton")
-myElement.onclick = function () {
-    window.alert("I have been clicked.");
+window.onload = function () {
+    var myElement = document.getElementById("myButton")
+        myElement.onclick = function () {
+        window.alert("I have been clicked.");
+    }
 }
 
 // User Defined
 function btnOnClick() {
     window.alert("I have been clicked.")
 }
-var myOtherButton = window.document.getElementById("myOtherButton")
-myOtherButton.addEventListener("click", btnOnClick);
-
-// Call-Back Function
-var myOtherButton2 = window.document.getElementById("myOtherButton2")
-myOtherButton2.addEventListener("click", function() {
-    window.alert("I have been clicked.")
+window.addEventListener("load", function(){
+    var myOtherButton = window.document.getElementById("myOtherButton")
+    myOtherButton.addEventListener("click", btnOnClick);
 });
 
-// Event Listners
-function init() {
-    "use strict";
-    var myOtherButton3 = window.document.getElementById("myOtherButton3")
-    myOtherButton3.addEventListener("click", function() {
+// Call-Back Function
+window.addEventListener("load", function(){
+    var myOtherButton2 = window.document.getElementById("myOtherButton2")
+        myOtherButton2.addEventListener("click", function() {
         window.alert("I have been clicked.")
     });
-}
-
-window.addEventListener("load", init);
+})
